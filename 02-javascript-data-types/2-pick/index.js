f73ba9adf8ac6f2223425223bf67fd70bc8c71c6
Mikhail Shakhanov <mikhail.shakhanov@gmail.com>
2021-02-01 08:09:26 +0300

    Добавлены задачи sortStrings, pick, omit

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
-};
+    let objCopy = {};
+    //создаем копию объекта
+    Object.assign(objCopy, obj);
+    //для каждого ключа объекта, который не входит в перечень, удаляем такое ключ: значение
+    Object.keys(objCopy).forEach( (item) => {
+      if (!fields.includes(item)) delete objCopy[item];
+    });
+    
+    return objCopy;
+  };
