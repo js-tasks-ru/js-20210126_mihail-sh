a2883886497caee9c8758ff9cbc5529f1add7518
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-12-09 01:09:31 +0300

    Added task SortableTable v3 с динамической загрузкой данных и "infinity scroll"

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/__mocks__/products-data.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/__mocks__/products-data.js
@@ -1,4 +1,4 @@
-export default [
+export const products = [
   {
     'id': '105-planset-apple-ipad-pro-wi-ficellular-64-gb--lte-seryj',
     'title': '10.5" Планшет Apple iPad Pro Wi-Fi+Cellular 64 ГБ , LTE серый',
@@ -1006,5 +1006,84 @@ export default [
     'price': 557,
     'rating': 4.3,
     'discount': 0
+  }
+];
+
+export const bestsellers = [
+  {
+    "id": "kabel-mezblocnyj-mystery-mpro-52",
+    "title": "Кабель межблочный MYSTERY MPRO-5.2",
+    "description": "Коаксиальный межблочный кабель Mystery MPRO-5.2 создан для качественного подключения 2-х канального усилителя. Максимальная протяженность соединительной линии — 5 метров.  Изоляция из ПВХ-пластика защищает проводник от потертостей и механических повреждений. Двойной экран и технология «витой пары» позволяет добиться отсутствия шумов. Проводник из бескислородной меди передает сигнал без падения уровня и искажений. Позолоченные контакты разъемов гарантируют отсутствие проблем при подключении.",
+    "brand": "MYSTERY",
+    "quantity": 24,
+    "subcategory": {
+      "id": "avtozvuk",
+      "title": "Автозвук",
+      "count": 24,
+      "category": {
+        "id": "avtotovary",
+        "title": "Автотовары",
+        "count": 56,
+        "weight": 785
+      },
+      "weight": 795
+    },
+    "status": 1,
+    "characteristics": [
+      {
+        "title": "Общие параметры",
+        "items": [
+          {
+            "name": "Тип",
+            "value": "кабель межблочный"
+          },
+          {
+            "name": "Модель",
+            "value": "MYSTERY MPRO-5.2",
+            "isExtended": true
+          }
+        ]
+      },
+      {
+        "title": "Основные характеристики",
+        "items": [
+          {
+            "name": "Длина",
+            "value": "5 м"
+          },
+          {
+            "name": "Разъемы",
+            "value": "2RCA (m) - 2RCA (m)"
+          },
+          {
+            "name": "Управляющий провод",
+            "value": "есть"
+          },
+          {
+            "name": "Дополнительно",
+            "value": "бескислородная медь, двойной экран",
+            "isExtended": true
+          }
+        ]
+      }
+    ],
+    "images": [
+      {
+        "url": "https://c.dns-shop.ru/thumb/st4/fit/wm/2000/1500/8f347ccae32e73d1d3dd74cc6c9c6a7f/6ed9ce1fae3d8ebbf6ceb1980dd98f72a4ad1161c7df338f7407cb615effa8cc.jpg",
+        "source": "6ed9ce1fae3d8ebbf6ceb1980dd98f72a4ad1161c7df338f7407cb615effa8cc.jpg"
+      },
+      {
+        "url": "https://c.dns-shop.ru/thumb/st4/fit/wm/2000/1500/ae7f49b919a8f5c5a2e08b365ffa5526/d331faddc2a0b9838d7465994bbc7c7beb2872a881477adb04a9671dc3bb7d04.jpg",
+        "source": "d331faddc2a0b9838d7465994bbc7c7beb2872a881477adb04a9671dc3bb7d04.jpg"
+      },
+      {
+        "url": "https://c.dns-shop.ru/thumb/st4/fit/wm/1500/2000/1bd4d951c70ac0fac5d728ac02b164de/9a76306be55a2a99dfc5e85a559f544cfa2eeb36edd154099c454658cef4b58e.jpg",
+        "source": "9a76306be55a2a99dfc5e85a559f544cfa2eeb36edd154099c454658cef4b58e.jpg"
+      }
+    ],
+    "price": 9,
+    "rating": 3.8,
+    "discount": 0,
+    "sales": 36
   }
 ];
